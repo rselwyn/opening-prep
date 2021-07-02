4e538a98dcd836f61afd0892e946e89c24be105c
Robbie Selwyn <rselwyn@nvidia.com>
2021-07-02 00:41:32 -0400

    Pull opening tree

--- backend.py
+++ backend.py
@@ -1,5 +1,8 @@
 import chess.pgn
 import io
+import json
+
+import urllib.parse
 
 def dfs_move_tree(visited, current_node, line_list):
 	if len(current_node.variations) == 0:
@@ -16,5 +19,10 @@ def load_file_to_lines(pgntext):
 	for i in prep.variations:
 		dfs_move_tree([], i, line_list)
 	print(line_list)
+
+def lookup_line_masters(fen):
+	req_url = url + urllib.parse.urlencode({"fen": fen})
+	resp = http.request("GET", req_url).data
+	json_back = json.loads(resp)
 
 # load_file_to_lines(open("ponzi.pgn"))
