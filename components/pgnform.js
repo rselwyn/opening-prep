12fabf814c8b6ea53cf854d84e63bad62f669323
Adam Siwiec <asiwiec@nvidia.com>
2021-07-01 23:25:47 -0400

    init

---
+++ components/pgnform.js
@@ -0,0 +1,56 @@
+
+const PGNForm = ({ onSubmit }) => (
+  <form onSubmit={onSubmit}>
+    <label>
+      <span>PGN</span>
+      <input type="text" name="pgn" required />
+    </label>
+
+    <div className="submit">
+          <button type="submit">Submit PGN</button>
+    </div>
+
+
+    <style jsx>{`
+      form,
+      label {
+        display: flex;
+        flex-flow: column;
+      }
+      label > span {
+        font-weight: 600;
+      }
+      input {
+        padding: 8px;
+        margin: 0.3rem 0 1rem;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+      }
+      .submit {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        justify-content: space-between;
+      }
+      .submit > a {
+        text-decoration: none;
+      }
+      .submit > button {
+        padding: 0.5rem 1rem;
+        cursor: pointer;
+        background: #fff;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+      }
+      .submit > button:hover {
+        border-color: #888;
+      }
+      .error {
+        color: brown;
+        margin: 1rem 0 0;
+      }
+    `}</style>
+  </form>
+)
+
+export default PGNForm
---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import { useUser } from '../lib/hooks'
+import React, { useState } from 'react';
+import Layout from '../components/layout'
+import PGNForm from '../components/pgnform'
+
+const Home = () => {
+
+    const [tree, setTree] = useState({});
+
+
+  async function uploadPGN(e) {
+    e.preventDefault()
+
+    // if (errorMsg) setErrorMsg('')
+
+    const body = {
+      pgn: e.currentTarget.pgn.value,
+    }
+      try {
+      const res = await fetch("/api/pgn", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+
+      console.log(res.json());
+
+      setTree(res.json());
+      
+    } catch (error) {
+      console.error('An unexpected error happened occurred:', error)
+    }
+  }
+
+  return (
+    <Layout>
+      <h1>PGN Juicer</h1>
+
+    
+      <PGNForm onSubmit={uploadPGN}/>
+
+      <style jsx>{`
+        li {
+          margin-bottom: 0.5rem;
+        }
+        pre {
+          white-space: pre-wrap;
+          word-wrap: break-word;
+        }
+      `}</style>
+    </Layout>
+  )
+}
+
+export default Home
