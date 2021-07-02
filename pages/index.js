defebf8d2b4748404dbc295b249bd46912c69d2e
Adam Siwiec <asiwiec@nvidia.com>
2021-07-02 01:27:50 -0400

    init

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,72 @@ import React, { useState } from 'react';
 import Layout from '../components/layout'
 import PGNForm from '../components/pgnform'
 import Chessground from '../components/chessground'
+import Chess, { ChessInstance, ShortMove } from "chess.js";
+
+
+
 import 'react-chessground/dist/styles/chessground.css'
 
 const Home = () => {
 
-    const [tree, setTree] = useState({});
+  const [tree, setTree] = useState({});
+ const [chess, setChess] = useState(new Chess())
+  const [pendingMove, setPendingMove] = useState()
+  const [selectVisible, setSelectVisible] = useState(false)
+  const [fen, setFen] = useState("")
+  const [lastMove, setLastMove] = useState()
+  const onMove = (from, to) => {
+    const moves = chess.moves({ verbose: true })
+    for (let i = 0, len = moves.length; i < len; i++) { /* eslint-disable-line */
+      if (moves[i].flags.indexOf("p") !== -1 && moves[i].from === from) {
+        setPendingMove([from, to])
+        setSelectVisible(true)
+        return
+      }
+    }
+    if (chess.move({ from, to, promotion: "x" })) {
+      setFen(chess.fen())
+      setLastMove([from, to])
+      setTimeout(randomMove, 500)
+    }
+  }
 
+  const randomMove = () => {
+    const moves = chess.moves({ verbose: true })
+    const move = moves[Math.floor(Math.random() * moves.length)]
+    if (moves.length > 0) {
+      chess.move(move.san)
+      setFen(chess.fen())
+      setLastMove([move.from, move.to])
+    }
+  }
+
+  const promotion = e => {
+    const from = pendingMove[0]
+    const to = pendingMove[1]
+    chess.move({ from, to, promotion: e })
+    setFen(chess.fen())
+    setLastMove([from, to])
+    setSelectVisible(false)
+    setTimeout(randomMove, 500)
+  }
+
+  const turnColor = () => {
+    return chess.turn() === "w" ? "white" : "black"
+  }
+
+  const calcMovable = () => {
+    const dests = new Map()
+    chess.SQUARES.forEach(s => {
+      const ms = chess.moves({ square: s, verbose: true })
+      if (ms.length) dests.set(s, ms.map(m => m.to))
+    })
+    return {
+      free: false,
+      dests,
+      color: "white"
+    }
+  }
 
   async function uploadPGN(e) {
     e.preventDefault()
@@ -41,8 +101,16 @@ const Home = () => {
     
       <PGNForm onSubmit={uploadPGN}/>
 
-      <Chessground viewOnly={true}/>
-
+<Chessground
+          width="38vw"
+          height="38vw"
+          turnColor={turnColor()}
+          movable={calcMovable()}
+          lastMove={lastMove}
+          fen={fen}
+          onMove={onMove}
+          style={{ margin: "auto" }}
+        />
 
       <style jsx>{`
         li {
