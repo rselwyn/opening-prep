9cf38459e29ba572d6e511e9bd0efbf159e6540d
Adam Siwiec <asiwiec@nvidia.com>
2021-07-03 11:00:49 -0400

    added undo button

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,28 @@
-import { useUser } from '../lib/hooks'
-import React, { useState, useRef, useEffect } from 'react';
-import Layout from '../components/layout'
-import PGNForm from '../components/pgnform'
-import Chessground from '../components/chessground'
+import { useUser } from "../lib/hooks";
+import React, { useState, useRef, useEffect } from "react";
+import Layout from "../components/layout";
+import PGNForm from "../components/pgnform";
+import Chessground from "../components/chessground";
 import Chess, { ChessInstance, ShortMove } from "../lib/chess.js";
-import { Button, Container } from '@material-ui/core';
-import 'react-chessground/dist/styles/chessground.css'
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Player from '../components/player';
-
+import { Button, Container } from "@material-ui/core";
+import "react-chessground/dist/styles/chessground.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Player from "../components/player";
 
 const Home = () => {
-
   const [tree, setTree] = useState([]);
- const [chess, setChess] = useState(new Chess())
-  const [pendingMove, setPendingMove] = useState()
-  const [selectVisible, setSelectVisible] = useState(false)
-  const [fen, setFen] = useState("")
-  const [lastMove, setLastMove] = useState()
+  const [chess, setChess] = useState(new Chess());
+  const [pendingMove, setPendingMove] = useState();
+  const [selectVisible, setSelectVisible] = useState(false);
+  const [fen, setFen] = useState("");
+  const [lastMove, setLastMove] = useState();
   const [pgn, setPgn] = useState(false);
 
-
-
-    useInterval(() => {
-        if (pgn) {
-
+  useInterval(() => {
+    if (pgn) {
       move();
-        }
-    }, 500);
-  
-
+    }
+  }, 500);
 
   function useInterval(callback, delay) {
     const savedCallback = useRef();
@@ -48,86 +41,86 @@ const Home = () => {
     }, [delay]);
   }
 
-
   const undo = () => {
-    console.log("undo")
-    chess.undo()
-    setFen(chess.fen())
-  }
+    console.log("undo");
+    chess.undo();
+    setFen(chess.fen());
+  };
 
   const move = () => {
-
-    let from = tree[0].substring(0,2)
-    let to = tree[0].substring(2)
+    let from = tree[0].substring(0, 2);
+    let to = tree[0].substring(2);
     if (chess.move({ from, to, promotion: "x" })) {
       setFen(chess.fen());
       setLastMove([from, to]);
       setTree(tree.slice(1));
     }
-  }
+  };
 
   const onMove = (from, to) => {
-    console.log(from, to)
-    const moves = chess.moves({ verbose: true })
-    for (let i = 0, len = moves.length; i < len; i++) { /* eslint-disable-line */
+    console.log(from, to);
+    const moves = chess.moves({ verbose: true });
+    for (let i = 0, len = moves.length; i < len; i++) {
+      /* eslint-disable-line */
       if (moves[i].flags.indexOf("p") !== -1 && moves[i].from === from) {
-        setPendingMove([from, to])
-        setSelectVisible(true)
-        return
+        setPendingMove([from, to]);
+        setSelectVisible(true);
+        return;
       }
     }
     if (chess.move({ from, to, promotion: "x" })) {
-      setFen(chess.fen())
-      setLastMove([from, to])
-      setTimeout(randomMove, 500)
+      setFen(chess.fen());
+      setLastMove([from, to]);
+      setTimeout(randomMove, 500);
     }
-  }
+  };
 
   const randomMove = () => {
-    const moves = chess.moves({ verbose: true })
+    const moves = chess.moves({ verbose: true });
     // const move = moves[Math.floor(Math.random() * moves.length)]
     // if (moves.length > 0) {
     //   chess.move(move.san)
     //   setFen(chess.fen())
     //   setLastMove([move.from, move.to])
     // }
-  moves.forEach(move => console.log(move.san));
-  const result = moves.filter((move) => move.san == tree[0]);
-  console.log(result)
-  chess.move(result[0].san)
-  setFen(chess.fen())
-  setLastMove([result[0].from, result[0].to]);
+    moves.forEach((move) => console.log(move.san));
+    const result = moves.filter((move) => move.san == tree[0]);
+    console.log(result);
+    chess.move(result[0].san);
+    setFen(chess.fen());
+    setLastMove([result[0].from, result[0].to]);
+  };
 
-
-
-  }
-
-  const promotion = e => {
-    const from = pendingMove[0]
-    const to = pendingMove[1]
-    chess.move({ from, to, promotion: e })
-    setFen(chess.fen())
-    setLastMove([from, to])
-    setSelectVisible(false)
-    setTimeout(randomMove, 500)
-  }
+  const promotion = (e) => {
+    const from = pendingMove[0];
+    const to = pendingMove[1];
+    chess.move({ from, to, promotion: e });
+    setFen(chess.fen());
+    setLastMove([from, to]);
+    setSelectVisible(false);
+    setTimeout(randomMove, 500);
+  };
 
   const turnColor = () => {
-    return chess.turn() === "w" ? "white" : "black"
-  }
+    return chess.turn() === "w" ? "white" : "black";
+  };
 
   const calcMovable = () => {
-    const dests = new Map()
-    chess.SQUARES.forEach(s => {
-      const ms = chess.moves({ square: s, verbose: true })
-      if (ms.length) dests.set(s, ms.map(m => m.to))
-    })
+    const dests = new Map();
+    chess.SQUARES.forEach((s) => {
+      const ms = chess.moves({ square: s, verbose: true });
+      if (ms.length)
+        dests.set(
+          s,
+          ms.map((m) => m.to)
+        );
+    });
     return {
       free: false,
       dests,
-      color: "white"
-    }
-  }
+      color: "white",
+    };
+  };
 
   async function readFileAsync(file) {
     return new Promise((resolve, reject) => {
@@ -144,34 +137,32 @@ const Home = () => {
   }
 
   async function uploadPGN(e) {
-
     let body = await readFileAsync(e[0]);
 
-    console.log(body)
+    console.log(body);
 
-      let res = await fetch("https://opening-backend.herokuapp.com/pgns", {
-        method: 'POST',
-        headers: { 'Content-Type': 'text/plain' },
-        body: body,
-      })
+    let res = await fetch("https://opening-backend.herokuapp.com/pgns", {
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: body,
+    });
 
+    let data = await res.json();
 
-      let data = await res.json()
-
-      console.log(data.lines[0])
-      setTree(data.lines[0])
-      setPgn(true);
+    console.log(data.lines[0]);
+    setTree(data.lines[0]);
+    setPgn(true);
   }
 
-  return (<>
-    <CssBaseline/>
-    <Container maxWidth="lg">
-      <h1>PGN Juicer</h1>
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <h1>PGN Juicer</h1>
 
-    
-      <PGNForm onSubmit={uploadPGN}/>
+        <PGNForm onSubmit={uploadPGN} />
 
-<Chessground
+        <Chessground
           width="38vw"
           height="38vw"
           turnColor={turnColor()}
@@ -182,19 +173,20 @@ const Home = () => {
           style={{ margin: "auto" }}
         />
 
-        <Player/>
+        <Player />
 
+        <style jsx>{`
+          li {
+            margin-bottom: 0.5rem;
+          }
+          pre {
+            white-space: pre-wrap;
+            word-wrap: break-word;
+          }
+        `}</style>
+      </Container>{" "}
+    </>
+  );
+};
 
-      <style jsx>{`
-        li {
-          margin-bottom: 0.5rem;
-        }
-        pre {
-          white-space: pre-wrap;
-          word-wrap: break-word;
-        }
-      `}</style>
-    </Container>  </>)
-}
-
-export default Home
+export default Home;
